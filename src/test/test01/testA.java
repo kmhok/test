ab2579089ead73b6208c0c9d3f8c3749a88ab27e
DESKTOP-D1K92A6\pc <kmhok@naver.com>
2021-11-29 11:36:36 +0900

    This is Commit

---
+++ src/test/test01/testA.java
@@ -0,0 +1,17 @@
+package test.test01;
+
+public class testA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String te = "f";
+		if(te=="a") {
+			System.out.println("안녕 민호.");
+		}else if(te=="b") {
+			System.out.println("안녕.");
+		}else {
+			System.out.println("안녕아무것도아니야");
+		}
+	}
+
+}
