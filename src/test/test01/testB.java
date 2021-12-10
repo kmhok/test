9b44ade69a01fcce4ed4089cdbb1d4dc77f819bc
kmhok <kmhok@naver.com>
2021-12-10 13:57:13 +0900

    Update testB.java

--- src/test/test01/testB.java
+++ src/test/test01/testB.java
@@ -4,7 +4,7 @@ public class testB {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("hellodBBBBCDDZ2");
+		System.out.print("hellodBBBBCDDZ27");
 	}
 
 }
